c76af7b9c0ef1926b8faa6aa7b18ba03d3955f65
Boninsegna Fabrizio <fabrizio.boninsegna@phd.unipd.it>
2025-01-26 18:04:53 +0100

    Start tensor_top1.rs. To Do hash table construction.

---
+++ src/bin/tensor_top_1.rs
@@ -0,0 +1,63 @@
+use savefile::prelude::*;
+use savefile_derive::Savefile;
+use std::io;
+
+// Load cosine_similarity function from utils.rs
+use ann_rust::tensor_top1::TensorTop1;
+use ann_rust::utils::{generate_normal_gaussian_vectors, get_dot_product};
+
+#[derive(Savefile)]
+struct GaussianVectors {
+    vectors: Vec<Vec<f64>>,
+}
+
+fn main() {
+    let n = 100; // Number of vectors
+    let d = 100; // Dimension of each vector
+    let alpha: f64 = 0.9; // close point according to cosine similarity
+    let beta: f64 = 0.55; // far point according to cosine similarity
+
+    // Load file
+    let file_name = format!("data/dimension_{}/sample_{}.bin", d, n);
+    // Load or generate data
+    let data = match load_vectors(&file_name) {
+        Ok(data) => {
+            println!(
+                "Successfully loaded {} vectors from '{}'.",
+                data.vectors.len(),
+                file_name
+            );
+            data.vectors
+        }
+        Err(e) => {
+            eprintln!("Failed to load vectors: {}. Generating new vectors...", e);
+            let vectors = generate_normal_gaussian_vectors(n, d).unwrap();
+            vectors
+        }
+    };
+
+    // Create Top1 struct
+    let theta = (1. - alpha.powi(2)) * (1. - beta.powi(2)) / (1. - alpha * beta).powi(2);
+    let query = data[0].clone();
+    let tensor_top1 = TensorTop1::new(data, alpha, beta, theta);
+
+    // // Query the Top1 struct
+    // let result = top1.query(&query);
+    // match result {
+    //     Ok(Some(close_point)) => {
+    //         let dot_product = get_dot_product(&query, &close_point);
+    //         println!("Close point found with dot_product: {:?}", dot_product);
+    //     }
+    //     Ok(None) => {
+    //         println!("No close point found.");
+    //     }
+    //     Err(err) => {
+    //         eprintln!("Error: {:?}", err);
+    //     }
+    // }
+}
+
+fn load_vectors(file_name: &str) -> io::Result<GaussianVectors> {
+    load_file(file_name, 0)
+        .map_err(|e| io::Error::new(io::ErrorKind::NotFound, format!("Failed to load file: {}", e)))
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod top1;
 pub mod utils;
 pub mod close_top1;
+pub mod tensor_top1;
